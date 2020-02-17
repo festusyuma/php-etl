f288df8624b2d27f6f223928eeacefba9b6eec39
Agboma Festus <festusyuma@gmail.com>
2020-02-17 15:24:23 +0100

    safe keeping:

--- custom/result_student_assessment.php
+++ custom/result_student_assessment.php
@@ -4,56 +4,65 @@ require_once ('../Db.php');
 $old_db = Db::getInstance('sams_db_old');
 $new_db = Db::getInstance('sams_db_new');
 $rooms = $old_db->query("SELECT DISTINCT schoolCode, classCode FROM result ORDER BY schoolCode");
-
-
 $classAssessments = $old_db->query("SELECT DISTINCT school_schoolId, schoolClassCode, examCode FROM schoolclass WHERE examCode IS NOT NULL ORDER BY school_schoolId");
 
 if ($classAssessments) {
     $classAssessments = $classAssessments->fetch_all(1);
 
     foreach ($classAssessments as $classAssessment) {
-        $resultTypes = $old_db->query("SELECT * FROM config WHERE template='{$classAssessment['examCode']}' AND school_schoolId={$classAssessment['school_schoolId']}");
-
-        if ($resultTypes and $resultTypes->num_rows > 0) {
-            $resultTypes = $resultTypes->fetch_assoc();
-
-            $template_one = $new_db->query("SELECT * FROM assessment_type WHERE name='{$resultTypes['resultType1']}' and sid={$resultTypes['school_schoolId']}");
-            $template_two = $new_db->query("SELECT * FROM assessment_type WHERE name='{$resultTypes['resultType2']}' and sid={$resultTypes['school_schoolId']}");
-            $template_three = $new_db->query("SELECT * FROM assessment_type WHERE name='{$resultTypes['resultType3']}' and sid={$resultTypes['school_schoolId']}");
-            $template_four = $new_db->query("SELECT * FROM assessment_type WHERE name='{$resultTypes['resultType4']}' and sid={$resultTypes['school_schoolId']}");
-
-            $templates = [
-                $template_one,
-                $template_two,
-                $template_three,
-                $template_four
-            ];
-
-            var_dump($templates);
-        }
-    }
-}
-
-if ($rooms) {
-    $rooms = $rooms->fetch_all(1);
-    foreach ($rooms as $room) {
-        $school = $old_db->query("SELECT * FROM school WHERE schoolCode='{$room['schoolCode']}'");
-
+        $school = $old_db->query("SELECT * FROM school WHERE schoolId='{$classAssessment['school_schoolId']}'");
         if ($school and $school->num_rows > 0) {
             $school = $school->fetch_assoc();
-            $classRoom = $old_db->query("SELECT * FROM schoolClass WHERE schoolClassCode='{$room['classCode']}' AND school_schoolId={$school['schoolId']}");
-            if ($classRoom and $classRoom->num_rows > 0) {
-                $classRoom = $classRoom->fetch_assoc();
+            $templates = getTemplates($classAssessment['examCode'], $classAssessment['school_schoolId']);
 
-                if ($classRoom['examCode'] !== null) {
-                    $resultTypes = $old_db->query("SELECT * FROM config WHERE template='{$classRoom['examCode']}' AND school_schoolId={$school['schoolId']}");
+            if ($templates) {
+                $resultGroups = $old_db->query("SELECT DISTINCT
+                                                            room, session, subject, term 
+                                                        FROM result
+                                                        WHERE 
+                                                            classCode='{$classAssessment['schoolClassCode']}'
+                                                        AND
+                                                            schoolCode='{$school['schoolCode']}'
+                                                        AND
+                                                            room != 'NULL'");
 
-                    if ($resultTypes and $resultTypes->num_rows > 0) {
-                        $resultTypes = $resultTypes->fetch_assoc();
-                        var_dump($school['schoolCode'], $classRoom['schoolClassCode']);
+
+                if ($resultGroups) {
+                    foreach ($resultGroups as $resultGroup) {
+                        createAssessment($resultGroup, $templates);
                     }
                 }
             }
         }
     }
+}
+
+function getTemplates($examCode, $schoolId) {
+    $old_db = Db::getInstance('sams_db_old');
+    $new_db = Db::getInstance('sams_db_new');
+    $resultTypes = $old_db->query("SELECT * FROM config WHERE template='{$examCode}' AND school_schoolId={$schoolId}");
+
+    if ($resultTypes and $resultTypes->num_rows > 0) {
+        $resultTypes = $resultTypes->fetch_assoc();
+
+        $template_one = $new_db->query("SELECT * FROM assessment_type WHERE name='{$resultTypes['resultType1']}' and sid={$resultTypes['school_schoolId']}");
+        $template_two = $new_db->query("SELECT * FROM assessment_type WHERE name='{$resultTypes['resultType2']}' and sid={$resultTypes['school_schoolId']}");
+        $template_three = $new_db->query("SELECT * FROM assessment_type WHERE name='{$resultTypes['resultType3']}' and sid={$resultTypes['school_schoolId']}");
+        $template_four = $new_db->query("SELECT * FROM assessment_type WHERE name='{$resultTypes['resultType4']}' and sid={$resultTypes['school_schoolId']}");
+
+        $templates = [
+            $template_one,
+            $template_two,
+            $template_three,
+            $template_four
+        ];
+
+        return $templates;
+    }
+
+    return false;
+}
+
+function createAssessment($resultGroup, $templates) {
+    var_dump($resultGroup);
 }
