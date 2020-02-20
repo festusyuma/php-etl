f1587974b6ad65fd3d4fbda7c18f7afb654e0a2f
Agboma Festus <festusyuma@gmail.com>
2020-02-20 14:15:52 +0100

    assessment migration

--- custom/result_student_assessment.php
+++ custom/result_student_assessment.php
@@ -5,6 +5,7 @@ $old_db = Db::getInstance('sams_db_old');
 $new_db = Db::getInstance('sams_db_new');
 $rooms = $old_db->query("SELECT DISTINCT schoolCode, classCode FROM result ORDER BY schoolCode");
 $classAssessments = $old_db->query("SELECT DISTINCT school_schoolId, schoolClassCode, examCode FROM schoolclass WHERE examCode IS NOT NULL ORDER BY school_schoolId");
+$resultTypesCol = ['resulttype1', 'resulttype2', 'resulttype3', 'resulttype4'];
 
 if ($classAssessments) {
     foreach ($classAssessments as $classAssessment) {
@@ -12,13 +13,14 @@ if ($classAssessments) {
         if ($school and $school->num_rows > 0) {
             $school = $school->fetch_assoc();
             $templates = getTemplates($classAssessment['examCode'], $classAssessment['school_schoolId']);
+            $classCode = $classAssessment['schoolClassCode'];
 
             if ($templates) {
                 $resultGroups = $old_db->query("SELECT DISTINCT
                                                             room, session, subject, term 
                                                         FROM result
                                                         WHERE 
-                                                            classCode='{$classAssessment['schoolClassCode']}'
+                                                            classCode='{$classCode}'
                                                         AND
                                                             schoolCode='{$school['schoolCode']}'
                                                         AND
@@ -27,8 +29,22 @@ if ($classAssessments) {
 
                 if ($resultGroups) {
                     foreach ($resultGroups as $resultGroup) {
-                        $assessmentData = getAssessmentData($resultGroup, $school['schoolId'], $classAssessment['schoolClassCode']);
-                        var_dump($assessmentData);
+                        $assessmentData = getAssessmentData($resultGroup, $school['schoolId'], $classCode);
+                        $studentsAssessments = $old_db->query("SELECT * FROM result WHERE classCode='{$classCode}' AND room='{$assessmentData['class']['name']}' AND session='{$assessmentData['session']['description']}' AND term='{$assessmentData['event']['title']}' AND subject='{$assessmentData['subject']['code']}' AND schoolCode='{$school['schoolCode']}'");
+
+                        if ($studentsAssessments) {
+                            $studentsAssessments = $studentsAssessments->fetch_all(1);
+                            foreach ($templates as $index => $template) {
+                                if ($template and $template != null) {
+                                    $assessment = createAssessment($assessmentData, $template);
+                                    if ($assessment) {
+                                        foreach ($studentsAssessments as $studentsAssessment) {
+                                            //todo populate score for assessment
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
@@ -99,7 +115,8 @@ function getAssessmentData($resultGroup, $schoolId, $classCode) {
                         return [
                             'class' => $class,
                             'subject' => $subject,
-                            'event' => $event
+                            'event' => $event,
+                            'session' => $session,
                         ];
                     }
                 }
@@ -110,8 +127,26 @@ function getAssessmentData($resultGroup, $schoolId, $classCode) {
     return false;
 }
 
-function createAssessments() {
+function createAssessment($assessmentData, $template) {
+    global $new_db;
 
+    $sid = $template['sid'];
+    $name = ucfirst(strtolower("{$template['name']} Assessment"));
+    $total_score = $template['total_score'];
+    $event_id = $assessmentData['event']['id'];
+    $class_id = $assessmentData['class']['id'];
+    $subject_id = $assessmentData['subject']['id'];
+    $type_id = $template['id'];
+
+    $assessment = $new_db->query("INSERT INTO assessment (sid, name, total_score, event_id, sclass_id, subject_id, type_id) VALUES ({$sid}, '{$name}', {$total_score}, {$event_id}, {$class_id}, {$subject_id}, {$type_id})");
+
+    if ($assessment) {
+        $id = $new_db->insert_id;
+        $assessment = $new_db->query("SELECT * FROM assessment WHERE id={$id}");
+        return $assessment->fetch_assoc();
+    }
+
+    return false;
 }
 
 function mapStudentAssessment() {
